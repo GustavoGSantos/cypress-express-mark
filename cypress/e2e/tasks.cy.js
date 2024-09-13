8173ed60ee96597dd6b0f0c64123b6efa71b6128
Gustavo Gimenez <gustavogimenezs@icloud.com>
2024-09-13 11:28:50 +0100

    fill the input field

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,1 +1,10 @@
 /// <reference types="cypress" />
+
+describe('tasks', ()=> {
+    
+    it('Should register a new task', ()=>{
+        cy.visit('http://localhost:3000')
+
+        cy.get('input[placeholder="Add a new Task"]').type('Read a node.js book')
+    })
+})
