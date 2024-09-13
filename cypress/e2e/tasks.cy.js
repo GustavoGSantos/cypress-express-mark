da3e02c1bbb4da88f79ed36524e5d7c225524ed9
Gustavo Gimenez <gustavogimenezs@icloud.com>
2024-09-13 14:20:07 +0100

    Automated test to register a task

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,10 +1,19 @@
 /// <reference types="cypress" />
 
-describe('tasks', ()=> {
-    
-    it('Should register a new task', ()=>{
+describe('tasks', () => {
+
+    it('Should register a new task', () => {
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: { name: 'Read a node.js book' }
+        }).then(response => {
+            expect(response.status).to.eq(204)
+        })
+
         cy.visit('http://localhost:3000')
 
         cy.get('input[placeholder="Add a new Task"]').type('Read a node.js book')
+        cy.contains('button', 'Create').click()
     })
 })
