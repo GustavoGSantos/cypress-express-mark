02821c2945ef94739b9f5972369651c8b4c74a16
Gustavo Gimenez <gustavogimenezs@icloud.com>
2024-09-13 14:39:27 +0100

    Validating task is created

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -15,5 +15,8 @@ describe('tasks', () => {
 
         cy.get('input[placeholder="Add a new Task"]').type('Read a node.js book')
         cy.contains('button', 'Create').click()
+
+        cy.contains('main > div > p', 'Read a node.js book')
+            .should('be.visible')
     })
 })
