d25613569b2c66dafcab5b392217d9a6316a437d
Gustavo Gimenez <gustavogimenezs@icloud.com>
2024-09-16 14:51:23 +0100

    Implementing Allure report

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,17 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+const { allureCypress } = require("allure-cypress/reporter");
+
+module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    env:{
+    env: {
       apiUrl: 'http://localhost:3333'
     },
     viewportWidth: 1920,
     viewportHeight: 1080,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureCypress(on, config);
+      return config
     },
   },
-};
+});
